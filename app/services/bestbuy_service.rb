898796832d950ac340090b42b778ffc098a2eb98
Sarah Kirk <kirk710@gmail.com>
2017-07-26 12:12:17 -0600

    might need to refactor API call to get total results with pagination

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -3,5 +3,10 @@ class BestbuyService
   def self.find_stores(zip)
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&pageSize=10&apiKey=#{ENV["best_buy_api_key"]}")
     results = JSON.parse(response.body, symbolize_names: true)[:stores]
+    binding.pry
+  end
+
+  def total_resutls(zip)
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&pageSize=10&apiKey=#{ENV["best_buy_api_key"]}")
   end
 end
