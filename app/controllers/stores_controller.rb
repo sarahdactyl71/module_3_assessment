c38fb643efc737d0edc9284e1bc0fdc8271d0785
Sarah Kirk <kirk710@gmail.com>
2017-07-26 12:02:58 -0600

    makes a model to break logic out

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,22 +1,10 @@
 class StoresController < ApplicationController
 
-  # attr_reader :name, :city, :distance, :phone, :type
-  #
-  # def initialize(info)
-  #   @name = info["longName"]
-  #   @city = info["city"]
-  #   @distance = info["distance"]
-  #   @phone = info["phone"]
-  #   @type = info["storeType"]
-  # end
-
   def index
     if params[:search]
       zip = params[:search]
-      BestbuyService.find_stores(zip)
-      @stores.map do |info|
-        new(info)
-      end
+      Bestbuy.find_stores(zip)
+      # @stores = BestbuyService.find_stores(zip)
     end
   end
 
--- app/models/bestbuy.rb
+++ app/models/bestbuy.rb
@@ -1,2 +1,16 @@
 class Bestbuy
+
+  attr_reader :name, :city, :distance, :phone, :type
+
+  def initialize(info)
+    @name = info["longName"]
+    @city = info["city"]
+    @distance = info["distance"]
+    @phone = info["phone"]
+    @type = info["storeType"]
+  end
+
+  def self.find_stores(zip)
+    BestbuyService.find_stores(zip)
+  end
 end
