750bbd413918ee6b098c7c72dbe0abbc3eeb2fa8
Sarah Kirk <kirkse710@gmail.com>
2017-07-31 13:39:30 -0600

    figured out how to query each page

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -1,7 +1,7 @@
 class BestbuyService
 
   def self.find_stores(zip)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&apiKey=#{ENV["best_buy_api_key"]}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&page=1&apiKey=#{ENV["best_buy_api_key"]}")
     results = JSON.parse(response.body, symbolize_names: true)[:stores]
   end
 
