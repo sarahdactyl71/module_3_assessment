92dec8543705af9891aea681255729c9b6113c4b
Sarah Kirk <kirkse710@gmail.com>
2017-07-31 13:13:14 -0600

    finally got everything debugged and ready to go

--- spec/features/best_buy_api_spec.rb
+++ spec/features/best_buy_api_spec.rb
@@ -18,8 +18,6 @@ RSpec.describe "best buy api" do
   end
 end
 
-# 2. Consume Best Buy's API
-#
 # As a user
 # When I visit "/"
 # And I fill in a search box with "80202" and click "search"
@@ -28,3 +26,17 @@ end
 # And I should see a message that says "17 Total Stores"
 # And I should see exactly 10 results (There are 17 stores within 25 miles. We want to display 10 on this page and 7 on the next. Get the first page to work first from start to finish and worry about pagination later.)
 # And I should see the long name, city, distance, phone number and store type for each of the 10 results
+#
+# (Additional Functionality)
+#
+# And I should see pagination links below the search results for the number of pages (2 in this case)
+# And `1` is my current page but isn't a clickable link
+# And `2` is a clickable link
+# When I click `2`
+# Then I should be taken to the next page of search results
+# Then my current path should be "/search" (ignoring params)
+# And in the params I should see `page=2`
+# And I should see stores within 25 miles
+# And I should see a message that says "17 Total Stores"
+# And I should see the next 6 results
+# And I should see the long name, city, distance, phone number and store type for each of the next 6 results
