b472699ad9e22208fbe0e72e24d78ee380dce262
Sarah Kirk <kirkse710@gmail.com>
2017-07-31 14:12:34 -0600

    may be onto something hacky and gross with getting the next page

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,6 +3,9 @@ class StoresController < ApplicationController
   def index
     if params[:search]
       zip = params[:search]
+      if params[:page]
+        response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&page=2&apiKey=#{ENV["best_buy_api_key"]}")
+      end
       @stores = Bestbuy.find_stores(zip)
       @total = Bestbuy.total_stores(zip)
     end
--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -1,9 +1,6 @@
 class BestbuyService
 
   def self.find_stores(zip)
-    # if params[:page2]
-    #   response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&page=2&apiKey=#{ENV["best_buy_api_key"]}")
-    # end
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&page=1&apiKey=#{ENV["best_buy_api_key"]}")
     results = JSON.parse(response.body, symbolize_names: true)[:stores]
   end
@@ -12,5 +9,9 @@ class BestbuyService
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&apiKey=#{ENV["best_buy_api_key"]}")
     results = JSON.parse(response.body, symbolize_names: true)[:total]
   end
+
+  # if params[:page]
+  #   response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&page=2&apiKey=#{ENV["best_buy_api_key"]}")
+  # end
 
 end
